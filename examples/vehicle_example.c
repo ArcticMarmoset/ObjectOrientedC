e7e39da681159f58477b7da0c8cee227d2188564
arctic-marmoset <45583929+arctic-marmoset@users.noreply.github.com>
2020-12-25 13:49:11 -0500

    Update vehicle_example.c
    Add expected outputs as comments

--- examples/vehicle_example.c
+++ examples/vehicle_example.c
@@ -12,9 +12,9 @@ int main(void)
         Car_Ctor(car);
         vehicle = (IVehicle *)car;
     }
-    vehicle->vtable->Start(vehicle);
-    vehicle->vtable->Go(vehicle);
-    vehicle->vtable->Stop(vehicle);
+    vehicle->vtable->Start(vehicle); // "You turn the key in your Unknown Car"
+    vehicle->vtable->Go(vehicle);    // "You set your Unknown Car to park"
+    vehicle->vtable->Stop(vehicle);  // "You hit the gas in your Unknown Car"
     vehicle->vtable->Dtor(vehicle);
     free(vehicle);
     {
@@ -22,9 +22,9 @@ int main(void)
         Bicycle_Ctor(bicycle);
         vehicle = (IVehicle *)bicycle;
     }
-    vehicle->vtable->Start(vehicle);
-    vehicle->vtable->Go(vehicle);
-    vehicle->vtable->Stop(vehicle);
+    vehicle->vtable->Start(vehicle); // "You hop on your Unknown Bicycle"
+    vehicle->vtable->Go(vehicle);    // "You start pedaling on your Unknown Bicycle"
+    vehicle->vtable->Stop(vehicle);  // "You hop off your Unknown Bicycle"
     vehicle->vtable->Dtor(vehicle);
     free(vehicle);
 }
